189e7c2dbd11b7b53cee601ad32b069c83c0a26e
mlazzell17@gmail.com <MicheSi>
2020-10-02 09:47:18 -0700

    initial commit

---
+++ app/components/Main.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+
+export default function Main() {
+  return (
+    <View style={styles.container}>
+        <View style={styles.header}>
+            <Text style={styles.headerText}>To Do List</Text>
+        </View>
+        <ScrollView style={styles.scrollContainer}>
+
+        </ScrollView>
+        <View style={styles.footer}>
+            <TextInput 
+             style={styles.textInput}
+             placeholder='Enter to do'
+             placeholderTextColor='white'
+             underlineColorAndroid='transparent'
+             >
+
+            </TextInput>
+
+        </View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
