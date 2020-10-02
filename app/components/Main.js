0b32da14d7d79d87a5ed83a0665efe3c595288d5
mlazzell17@gmail.com <MicheSi>
2020-10-02 10:03:41 -0700

    applied default stylings

--- app/components/Main.js
+++ app/components/Main.js
@@ -15,9 +15,7 @@ export default function Main() {
              style={styles.textInput}
              placeholder='Enter to do'
              placeholderTextColor='white'
-             underlineColorAndroid='transparent'
-             >
-
+             underlineColorAndroid='transparent'>
             </TextInput>
 
         </View>
@@ -30,4 +28,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  header: {
+      backgroundColor: '#E91E63',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderBottomWidth: 10,
+      borderBottomColor: '#ddd'
+  },
+  headerText: {
+      color: 'white',
+      fontSize: 18,
+      padding: 26,
+  },
+  scrollContainer: {
+      flex: 1,
+      marginBottom: 100,
+  },
+  footer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      zIndex: 10,
+  },
+  textInput: {
+      alignSelf: 'stretch',
+      color: '#fff',
+      padding: 20,
+      backgroundColor: '#252525',
+      borderTopWidth: 2,
+      borderTopColor: '#ededed',
+  },
+  addButton: {
+      position: 'absolute',
+      zIndex: 11,
+      right: 20,
+      bottom: 90,
+      backgroundColor: '#E91E63',
+      width: 90,
+      height: 90,
+      borderRadius: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      elevation: 8,
+  },
+  addButtonText: {
+      color: '#fff',
+      fontSize: 24,
+  }
 });
